a5e193f7050aa2b6e42daa08ad8de5a3417475c2
kalayo <kalayo.work@gmail.com>
2014-12-04 21:56:27 -0600

    More readable analysis script.

--- run_analysis_draft.R
+++ run_analysis_draft.R
@@ -5,65 +5,66 @@
 
 
 # Download, read and merge data.
-	#data_set = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-	#download.file(data_set, dest = 'har_data.zip', method = 'curl')
-	#unzip('har_data.zip')
 	
-	subject_test = read.table(file = 'UCI HAR Dataset/test/subject_test.txt')
-	y_test = read.table(file = 'UCI HAR Dataset/test/y_test.txt')
+	#data_set = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+	#download.file(data_set, dest = 'har_data.zip', method = 'curl')	# Data set.
+	#unzip('har_data.zip')												
+	
+	y_test = read.table(file = 'UCI HAR Dataset/test/y_test.txt')		# Test data.
 	x_test = read.table(file = 'UCI HAR Dataset/test/x_test.txt')
-	x_test = data.frame(x_test, subject_test, y_test)
+	subject_test = read.table(file = 'UCI HAR Dataset/test/subject_test.txt')
+	x_test = data.frame(x_test, subject_test, y_test)					
 
-	subject_train = read.table(file = 'UCI HAR Dataset/train/subject_train.txt')
-	y_train = read.table(file = 'UCI HAR Dataset/train/y_train.txt')
+	y_train = read.table(file = 'UCI HAR Dataset/train/y_train.txt')		# Training data.
 	x_train = read.table(file = 'UCI HAR Dataset/train/x_train.txt')
-	x_train = data.frame(x_train, subject_train, y_train)
+	subject_train = read.table(file = 'UCI HAR Dataset/train/subject_train.txt')
+	x_train = data.frame(x_train, subject_train, y_train)				
 
-	features = read.table(file = 'UCI HAR Dataset/features.txt')
+	features = read.table(file = 'UCI HAR Dataset/features.txt')			# Additional files.
 	activity_labels = read.table(file = 'UCI HAR Dataset/activity_labels.txt')
 	
-	complete_set = rbind(x_train, x_test)
+	complete_set = rbind(x_train, x_test)								# Merge training and test data.
 	complete_set = as.tbl(complete_set)
 
+	merge_qc()															# QC after merge.
+		
 
-# Quality checks to verify integrity of data after merge.  q should be all true.
-	merge_qc()	
+# Subset means and standard deviations of variables.
 
-# Subset.
 	means = grep("-mean()", features$V2, fixed = T)
 	sd = grep("-std()", features$V2, fixed = T)
+	set_means_sd = select(complete_set, ncol(complete_set) - 1,
+								ncol(complete_set), means, sd)
 	
-	set_means_sd = select(complete_set, ncol(complete_set) - 1, ncol(complete_set), means, sd)
-	
-	subset_qc()
+	subset_qc()															# QC after subset.
 	
 
-# Relabel variables.
-	names(set_means_sd) = c('subject', 'activity',
-			as.character(features[means, 'V2']), as.character(features[sd, 'V2']))
+# Relabel and recode variables according to features and activity_labels.
 	
-	relabel_qc()
+	names(set_means_sd) = c('subject', 'activity',						# Relabel columns.
+			as.character(features[means, 'V2']),
+								as.character(features[sd, 'V2']))
+	
+	relabel_qc()														# QC after relabel.
 		
-	set_means_sd = mutate(set_means_sd, activity = activity_labels[activity, 2])
+	set_means_sd = mutate(set_means_sd, activity =						# Recode activities.
+								activity_labels[activity, 2])
 	
-	recode_qc()
+	recode_qc()															# QC after recode.
 	
 	
-
-# Build final data set.
+# Build final, tidy data set.
+	
 	grouped_means = summarise_each(group_by(set_means_sd, activity, subject), funs(mean))
 	filename = paste('grouped_means_', format(Sys.Date(), format = '%d%m%Y'), '.txt', sep = '')
 	write.table(grouped_means, file = filename, row.names = F)
 	
-	grouped_qc()
-			
-
-#Final qc.  Compare with analysis run on 04 Dec 2014.
+	grouped_qc()														# QC after summary.
+	final_qc()															# Final QC.
 	
-	final_qc()
-	
-	qlist = list(merge = merge, subset = subset, relabel = relabel,
-					recode = recode, grouped_mean = grouped, final = final)
+	qlist = list(merge = merge, subset = subset,						# Assemble QC tests.
+					relabel = relabel, recode = recode,
+					grouped_mean = grouped, final = final)
 	print(qlist)
 
 
